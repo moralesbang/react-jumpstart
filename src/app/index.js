13ce41f512b3aec2e2d053305243dfae6ffa338a
Juan Camilo Morales <camov1209@gmail.com>
2020-06-22 13:47:08 -0500

    Set up

---
+++ src/app/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div>
+      <h1>Jumpstart Boostrap</h1>
+      <code>Created by @moralesbang</code>
+    </div>
+  )
+}
+
+export default App;
